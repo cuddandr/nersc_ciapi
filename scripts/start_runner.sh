e1426b90a6f8220cade019d06d3751863047a210
cuddandr <andrew.cudd@colorado.edu>
2025-10-29 15:20:49 -0700

    Add webhook download to start_runner.sh
    Added step to download the webhook for the requested GH workflow job for
    runner validation and other information.

--- scripts/start_runner.sh
+++ scripts/start_runner.sh
@@ -1,32 +1,53 @@
 #!/bin/bash
+module load python/3.11
+CI_CLIENT_DIR=${PWD}
 
-webhook_db_id=${1}
+WEBHOOK_DB_ID=${1}
 runner_dir=$SCRATCH/temp
 
 mkdir -p ${runner_dir}
 gh_dir=$(mktemp -d -p ${runner_dir})
+
+VENV_NAME="env/"
+if [ ! -d "${VENV_NAME}" ]; then
+    echo "Creating Python venv at ${PWD}/${VENV_NAME}"
+    python3 -m venv ${VENV_NAME}
+    source ${VENV_NAME}/bin/activate
+    pip install --no-cache-dir --upgrade -r requirements.txt
+else
+    echo "Sourcing existing venv at ${PWD}/${VENV_NAME}"
+    source ${VENV_NAME}/bin/activate
+fi
+
+MONGODB_URI=""
+MONGODB_DB_NAME="github_webhooks"
+MONGODB_COLLECTION="webhooks"
+python3 scripts/get_webhook.py --uri $MONGODB_URI -d $MONGODB_DB_NAME -c $MONGODB_COLLECTION --field _id --value $WEBHOOK_DB_ID > ${gh_dir}/payload.json
+
 cd ${gh_dir}
+echo "Current dir: ${PWD}"
+repo=$(jq -r '.repository.full_name' payload.json)
+echo "${repo}"
 
-echo "${webhook_db_id}" > webhook_id.txt
-# python3 -m venv env
-# source env/bin/activate
-# pip install -r requirements
+echo "Downloading GH self-hosted runner..."
+curl -sS -o actions-runner-linux-x64-2.329.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.329.0/actions-runner-linux-x64-2.329.0.tar.gz
+echo "194f1e1e4bd02f80b7e9633fc546084d8d4e19f3928a324d512ea53430102e1d  actions-runner-linux-x64-2.329.0.tar.gz" | shasum -a 256 -c
+tar xzf ./actions-runner-linux-x64-2.329.0.tar.gz
 
-# python3 scripts/get_webhook.py
-cp ~/payload.json .
-repo=$(jq '.repository.full_name' payload.json)
-
-curl -o actions-runner-linux-x64-2.328.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.328.0/actions-runner-linux-x64-2.328.0.tar.gz
-echo "01066fad3a2893e63e6ca880ae3a1fad5bf9329d60e77ee15f2b97c148c3cd4e  actions-runner-linux-x64-2.328.0.tar.gz" | shasum -a 256 -c
-tar xzf ./actions-runner-linux-x64-2.328.0.tar.gz
-
+echo "Retrieving GH access token..."
 token=$(gh api \
   --method POST \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   /repos/${repo}/actions/runners/registration-token | jq -r '.token')
 
+echo "Configuring runner..."
 echo "${token}" >> token.txt
+# --name option to set the runner name
 ./config.sh --unattended --url https://github.com/${repo} --token ${token} --ephemeral --labels perlmutter,gpu
 
 echo "PAYLOAD_FILE=$(realpath payload.json)" >> .env
+
+echo "Starting job."
+# srun ./run.sh
+sbatch ${CI_CLIENT_DIR}/scripts/sbatch_runner.sh
